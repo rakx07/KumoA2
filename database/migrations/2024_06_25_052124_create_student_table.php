496e862571bb9ed7161b2f345169c95ca3615ced
janry <janryseran069407@gmail.com>
2024-06-26 14:48:37 +0800

    added side navbar for college import

---
+++ database/migrations/2024_06_25_052124_create_student_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStudentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('student', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('School_id')->nullable();
+            $table->string('FirstName')->nullable();
+            $table->string('MiddleName')->nullable();
+            $table->string('LastName')->nullable();
+            $table->string('Course')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('student');
+    }
+}
---
+++ database/migrations/2024_06_25_065201_create_department_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDepartmentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('department', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('code')->nullable();
+            $table->string('name')->nullable();
+            $table->string('college_id')->nullable();
+            $table->string('is_active')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('department');
+    }
+}
